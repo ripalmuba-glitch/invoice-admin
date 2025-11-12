a4bedc1ba25275411e09a437983c2a6d35c3bbbf
Rivaldianto <ripalmuba@gmail.com>
2025-11-12 21:37:04 +0700

    Configure TrustProxies and Force HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request; // <-- PASTIKAN IMPORT INI ADA
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,9 +11,22 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+
+        // --- BLOK KODE YANG KITA TAMBAHKAN ---
+        // Ini memberi tahu Laravel untuk mempercayai header
+        // dari load balancer Railway (untuk HTTPS)
+        $middleware->trustProxies(
+            proxies: '*', // Percayai semua proxy
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO |
+                     Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+        // --- AKHIR BLOK KODE ---
+
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
